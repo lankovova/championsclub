2aebda736fe125d0003a048edc63b4edc7ff497a
lankovova <lanko.vova@gmail.com>
2018-01-15 15:45:07 +0200

    indentetion refactoring

--- resources/assets/divslot/js/Components/JackpotBonus.js
+++ resources/assets/divslot/js/Components/JackpotBonus.js
@@ -1,49 +1,49 @@
 class JackpotBonus {
-	constructor(jElement, bElement, initValues = {jValue: 2000, bValue: 1000}) {
-		if (!jElement || !bElement) {
-			console.error('Jackpot or bonus element is not defined in constructor');
+    constructor(jElement, bElement, initValues = {jValue: 2000, bValue: 1000}) {
+        if (!jElement || !bElement) {
+            console.error('Jackpot or bonus element is not defined in constructor');
 
-			return;
-		}
+            return;
+        }
 
-		this.jNode = jElement;
-		this.bNode = bElement;
+        this.jNode = jElement;
+        this.bNode = bElement;
 
-		this.jValue = initValues.jValue;
-		this.bValue = initValues.bValue;
+        this.jValue = initValues.jValue;
+        this.bValue = initValues.bValue;
 
-		this.setJackpot(this.jValue);
-		this.setBonus(this.bValue);
+        this.setJackpot(this.jValue);
+        this.setBonus(this.bValue);
 
-		this.intervalId;
-	}
+        this.intervalId;
+    }
 
-	run() {
-		this.intervalId = setInterval(() => this.tick(), 100);
-	}
+    run() {
+        this.intervalId = setInterval(() => this.tick(), 100);
+    }
 
-	stop() {
-		clearInterval(this.intervalId);
-	}
+    stop() {
+        clearInterval(this.intervalId);
+    }
 
-	tick() {
-		this.jValue += 0.04;
-		this.bValue += 0.03;
+    tick() {
+        this.jValue += 0.04;
+        this.bValue += 0.03;
 
-		this.updateFields();
-	}
+        this.updateFields();
+    }
 
-	updateFields() {
-		this.setJackpot(this.jValue);
-		this.setBonus(this.bValue);
-	}
+    updateFields() {
+        this.setJackpot(this.jValue);
+        this.setBonus(this.bValue);
+    }
 
-	setJackpot(value) {
-		this.jNode.innerText = value.toFixed(2);
-	}
-	setBonus(value) {
-		this.bNode.innerText = value.toFixed(2);
-	}
+    setJackpot(value) {
+        this.jNode.innerText = value.toFixed(2);
+    }
+    setBonus(value) {
+        this.bNode.innerText = value.toFixed(2);
+    }
 }
 
 export default JackpotBonus;
