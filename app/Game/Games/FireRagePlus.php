82c1f7f44b1b6226b98fb2ccbc1311f5f9e1e489
AlexandrKrivosheev <alexandrkrivosheevmail@gmail.com>
2018-01-19 10:43:40 +0200

    game classes added

--- app/Game/Games/FireRagePlus.php
+++ app/Game/Games/FireRagePlus.php
@@ -4,23 +4,21 @@ namespace App\Game\Games;
 
 use App\Game\Games\Game;
 
-class BookOfWinner extends Game {
+class FireRagePlus extends Game {
     public static $reelsAmount = 5;
     public static $scatter = 1;
     public static $joker = 1;
     public static $freeSpinsAmout = 10;
-    public static $symbolsAmount = 10;
+    public static $symbolsAmount = 8;
     public static $paytable = [
-        [0, 0, 5, 25, 100],         // Symbol 1
-        [0, 0, 5, 40, 150],         // Symbol 2
-        [0, 0, 3, 30, 300],         // Symbol 3
-        [0, 5, 30, 100, 750],       // Symbol 4
-        [0, 0, 5, 25, 100],         // Symbol 5
-        [0, 0, 5, 40, 150],         // Symbol 6
-        [0, 10, 100, 1000, 5000],   // Symbol 7
-        [0, 0, 5, 25, 100],         // Symbol 8
-        [0, 5, 40, 400, 2000],      // Symbol 9
-        [0, 5, 30, 100, 750]        // Symbol 10
+        [ 0, 2, 5, 20, 100 ], //PAYTABLE FOR SYMBOL 1
+        [ 0, 0, 20, 100, 200 ],  //PAYTABLE FOR SYMBOL 2
+        [ 0, 0, 5, 20, 100 ],  //PAYTABLE FOR SYMBOL 3
+        [ 0, 0, 10, 50, 100 ],  //PAYTABLE FOR SYMBOL 4
+        [ 0, 0, 10, 50, 100 ],    //PAYTABLE FOR SYMBOL 5
+        [ 0, 0, 10, 100, 500 ],    //PAYTABLE FOR SYMBOL 6
+        [ 0, 0, 10, 50, 100 ],     //PAYTABLE FOR SYMBOL 7
+        [ 0, 0, 20, 100, 200 ]     //PAYTABLE FOR SYMBOL 8
     ];
 
     function __construct($game) {
