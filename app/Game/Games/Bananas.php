6e3ad4606fed81599ee50b3fea4cc6da64103785
AlexandrKrivosheev <alexandrkrivosheevmail@gmail.com>
2018-01-24 13:46:02 +0200

    сфзефшт пфьиду

--- app/Game/Games/Bananas.php
+++ app/Game/Games/Bananas.php
@@ -31,7 +31,7 @@ class Bananas extends Game {
     }
 
     public function spin() {
-        return $this->game->spin("checkForWinCombosScatterAsJoker");
+        return $this->game->spin("checkForWinCombos");
     }
 
     public function bonusSpin() {
