3e3b8ae019326616d4034bde5ccf1444847a06cd
AlexandrKrivosheev <alexandrkrivosheevmail@gmail.com>
2018-01-25 14:41:15 +0200

    carazy barment + fix scattter in settings

--- public/js/goldenharvest_settings.js
+++ public/js/goldenharvest_settings.js
@@ -37,7 +37,7 @@ var settings = {
         {"image": "8.png"},
         {"image": "9.png"},
         {"image": "10.png"},
-        {"image": "11.png"},
+        {"image": "11.png" , "isScatter": true},
         {"image": "12.png"},
         {"image": "13.png"}
     ]
