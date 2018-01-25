d6ab908f05f7d3a4092a74275fc8de3c8d69ccf5
lankovova <lanko.vova@gmail.com>
2018-01-25 17:11:19 +0200

    beautydolphins done

--- public/js/beautydolphins_settings.js
+++ public/js/beautydolphins_settings.js
@@ -36,7 +36,7 @@ var settings = {
         {"image": "7.png"},
         {"image": "8.png"},
         {"image": "9.png"},
-        {"image": "10.png"},
+        {"image": "10.png", "isScatter": true},
         {"image": "11.png"},
         {"image": "12.png"},
         {"image": "13.png"}
