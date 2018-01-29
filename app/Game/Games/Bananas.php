05080d0c9a4c996f4ad9d1c47c6398e81eb1ec5f
AlexandrKrivosheev <alexandrkrivosheevmail@gmail.com>
2018-01-29 20:55:54 +0200

    fix infinite loop and refactoring

--- app/Game/Games/Bananas.php
+++ app/Game/Games/Bananas.php
@@ -11,19 +11,19 @@ class Bananas extends Game {
     public static $freeSpinsAmount = 45;
     public static $symbolsAmount = 13;
     public static $paytable = [
-        [0,2,4,30,100], //PAYTABLE FOR SYMBOL 1
-        [0,0,4,30,100],  //PAYTABLE FOR SYMBOL 2
-        [0,0,10,50,120],  //PAYTABLE FOR SYMBOL 3
-        [0,10,250,2500,9000],  //PAYTABLE FOR SYMBOL 4
-        [0,2,4,20,500],    //PAYTABLE FOR SYMBOL 5
-        [0,0,20,70,250],    //PAYTABLE FOR SYMBOL 6
-        [0,0,4,30,100],     //PAYTABLE FOR SYMBOL 7
-        [0,0,10,50,120],     //PAYTABLE FOR SYMBOL 8
-        [0,0,20,70,250],     //PAYTABLE FOR SYMBOL 9
-        [0,0,20,100,400],     //PAYTABLE FOR SYMBOL 10
-        [0,0,4,30,100],     //PAYTABLE FOR SYMBOL 11
-        [0,2,30,120,800],     //PAYTABLE FOR SYMBOL 12
-        [0,2,30,120,800]     //PAYTABLE FOR SYMBOL 13
+        [0,2,4,30,100], //PAYTABLE FOR SYMBOL 0
+        [0,0,4,30,100],  //PAYTABLE FOR SYMBOL 1
+        [0,0,10,50,120],  //PAYTABLE FOR SYMBOL 2
+        [0,10,250,2500,9000],  //PAYTABLE FOR SYMBOL 3
+        [0,2,4,20,500],    //PAYTABLE FOR SYMBOL 4
+        [0,0,20,70,250],    //PAYTABLE FOR SYMBOL 5
+        [0,0,4,30,100],     //PAYTABLE FOR SYMBOL 6
+        [0,0,10,50,120],     //PAYTABLE FOR SYMBOL 7
+        [0,0,20,70,250],     //PAYTABLE FOR SYMBOL 8
+        [0,0,20,100,400],     //PAYTABLE FOR SYMBOL 9
+        [0,0,4,30,100],     //PAYTABLE FOR SYMBOL 10
+        [0,2,30,120,800],     //PAYTABLE FOR SYMBOL 11
+        [0,2,30,120,800]     //PAYTABLE FOR SYMBOL 12
     ];
 
     function __construct($game) {
