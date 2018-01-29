1fd1a7efbf01367fd3ebaefeacf0e1f450f70756
lankovova <lanko.vova@gmail.com>
2018-01-29 11:35:36 +0200

    Layout refactoring

--- resources/views/games/types/gameOld.blade.php
+++ resources/views/games/types/gameOld.blade.php
@@ -15,34 +15,33 @@
 
 
             <div class="main flex_center">
-                    <div class="header">
-                        </div>
+                <div class="header"></div>
                 <div class="alert" id="alert">
                     <div class="content flex_center"></div>
                 </div>
                 <div id="reels_wrapper" class="reels_wrapper"></div>
             </div>
 
-			<div class="gamble" id="gamble">
-				<div class="gamble__amount">
-					<div class="gamble__amount-text">gamble amount</div>
-					<div class="gamble__amount-value" id="gameAmountValue"></div>
-				</div>
+            <div class="gamble" id="gamble">
+                <div class="gamble__amount">
+                    <div class="gamble__amount-text">gamble amount</div>
+                    <div class="gamble__amount-value" id="gameAmountValue"></div>
+                </div>
 
-				<div class="gamble__to-win">
-					<div class="gamble__to-win-suit">
-						suit gamble to win
-						<span id="gambleToWinSuit"></span>
-						(x4)
-					</div>
-					<div class="gamble__to-win-color">
-						color gamble to win
-						<span id="gambleToWinColor"></span>
-						(x2)
-					</div>
-				</div>
+                <div class="gamble__to-win">
+                    <div class="gamble__to-win-suit">
+                        suit gamble to win
+                        <span id="gambleToWinSuit"></span>
+                        (x4)
+                    </div>
+                    <div class="gamble__to-win-color">
+                        color gamble to win
+                        <span id="gambleToWinColor"></span>
+                        (x2)
+                    </div>
+                </div>
 
-				<div class="button__suit-color button__red" id="red">
+                <div class="button__suit-color button__red" id="red">
                     <div class="overflow-layer">red</div>
                 </div>
                 <div class="button__suit-color button__black" id="black">
@@ -61,16 +60,16 @@
                     <div class="overflow-layer"></div>
                 </div>
 
-				<div class="card" id="card"></div>
-				<div class="card__suit card__spade" id="suitSpade"></div>
-				<div class="card__suit card__club" id="suitClub"></div>
-				<div class="card__suit card__heart" id="suitHeart"></div>
-				<div class="card__suit card__diamond" id="suitDiamond"></div>
-				<div class="previous-cards" id="previous-cards">
-					<div class="previous-cards__text">previous cards</div>
-					<div class="previous-cards__suits" id="previousCardsSuits"></div>
-				</div>
-			</div>
+                <div class="card" id="card"></div>
+                <div class="card__suit card__spade" id="suitSpade"></div>
+                <div class="card__suit card__club" id="suitClub"></div>
+                <div class="card__suit card__heart" id="suitHeart"></div>
+                <div class="card__suit card__diamond" id="suitDiamond"></div>
+                <div class="previous-cards" id="previous-cards">
+                    <div class="previous-cards__text">previous cards</div>
+                    <div class="previous-cards__suits" id="previousCardsSuits"></div>
+                </div>
+            </div>
 
             <div class="panel">
 
