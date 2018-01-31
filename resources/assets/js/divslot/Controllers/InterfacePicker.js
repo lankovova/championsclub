d4dd511292a65d2e9125bcab7f3a529054349896
lankovova <lanko.vova@gmail.com>
2018-01-31 15:50:15 +0200

    hotfix

--- resources/assets/js/divslot/Controllers/InterfacePicker.js
+++ resources/assets/js/divslot/Controllers/InterfacePicker.js
@@ -1,6 +1,6 @@
 import NormalInterfaceController from './NormalInterfaceController';
 import OldInterfaceController from './OldInterfaceController';
 
-const exportedInterface = (settings.gameType === 'old') ? OldInterfaceController : NormalInterfaceController;
+const exportedInterfaceController = (settings.gameType === 'old') ? OldInterfaceController : NormalInterfaceController;
 
 export default exportedInterfaceController;
