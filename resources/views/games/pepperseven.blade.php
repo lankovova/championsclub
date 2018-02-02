2a1614a5990c09fb6e3805e73d937eb13d8fbddd
AlexandrKrivosheev <alexandrkrivosheevmail@gmail.com>
2018-02-02 14:45:15 +0200

    helps to substitution games added

--- resources/views/games/pepperseven.blade.php
+++ resources/views/games/pepperseven.blade.php
@@ -3,7 +3,23 @@
 @section('css')
     <link href="{{asset('public/css/pepperseven.css')}}" rel=stylesheet type=text/css>
 @endsection
-    
+
+@section('help')
+
+    @component('games.parts.help')
+        @slot('helpValues')
+            <div data-symbols="3" class="help__prize_container"></div>
+            <div data-symbols="0" class="help__prize_container"></div>
+            <div data-symbols="2 3" class="help__prize_container"></div>
+            <div data-symbols="5" class="help__prize_container"></div>
+            <div data-symbols="4" class="help__prize_container"></div>
+            <div data-symbols="9" class="help__prize_container"></div>
+            <div data-symbols="6 7 8" class="help__prize_container"></div>
+        @endslot
+    @endcomponent
+
+@endsection
+
 @section('js')
     @parent
     <script src="{{asset('public/js/pepperseven_settings.js')}}"></script>
