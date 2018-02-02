ef3bba8bc17ed4fc9e0012284ba83b9d8ab749bd
AlexandrKrivosheev <alexandrkrivosheevmail@gmail.com>
2018-02-02 15:12:57 +0200

    free spins games added help

--- resources/views/games/goldenharvest.blade.php
+++ resources/views/games/goldenharvest.blade.php
@@ -4,7 +4,28 @@
     <link href="{{asset('public/css/goldenharvest.css')}}" rel=stylesheet type=text/css>
 @endsection
 
+@section('help')
 
+    @component('games.parts.help')
+        @slot('helpValues')
+            <div data-symbols="5 12" class="help__prize_container"></div>
+            <div data-symbols="5 12" data-joker class="help__prize_container"></div>
+            <div data-symbols="2 9" class="help__prize_container"></div>
+            <div data-symbols="2 9" data-joker class="help__prize_container"></div>
+            <div data-symbols="1 7 11" class="help__prize_container"></div>
+            <div data-symbols="1 7 11" data-joker class="help__prize_container"></div>
+            <div data-symbols="8" class="help__prize_container"></div>
+            <div data-symbols="10" class="help__prize_container"></div>
+            <div data-symbols="3 7" class="help__prize_container"></div>
+            <div data-symbols="3 7" data-joker class="help__prize_container"></div>
+            <div data-symbols="4" class="help__prize_container"></div>
+            <div data-symbols="4" data-joker class="help__prize_container"></div>
+            <div data-symbols="0" class="help__prize_container"></div>
+            <div data-symbols="0" data-joker class="help__prize_container"></div>
+        @endslot
+    @endcomponent
+
+@endsection
 @section('js')
     @parent
     <script src="{{asset('public/js/goldenharvest_settings.js')}}"></script>
