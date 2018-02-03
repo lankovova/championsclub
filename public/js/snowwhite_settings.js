7d4237863bf3662159e4c06f84ea95bbbc85f770
AlexandrKrivosheev <alexandrkrivosheevmail@gmail.com>
2018-02-03 15:47:03 +0200

    scatter settings added

--- public/js/snowwhite_settings.js
+++ public/js/snowwhite_settings.js
@@ -30,13 +30,13 @@ var settings = {
         {"image": "1.png"},
         {"image": "2.png"},
         {"image": "3.png"},
-        {"image": "4.png", "animation": "4Anim.png"},
+        {"image": "4.png"},
         {"image": "5.png"},
-        {"image": "6.png"},
-        {"image": "7.png"},
+        {"image": "6.png", "isScatter": true},
+        {"image": "7.png", "isScatter": true},
         {"image": "8.png"},
         {"image": "9.png"},
-        {"image": "10.png"},
+        {"image": "10.png", "isScatter": true},
         {"image": "11.png"},
         {"image": "12.png"}
     ]
