f82d909b3abf063d678225846aa541fe7083affc
AlexandrKrivosheev <alexandrkrivosheevmail@gmail.com>
2018-02-03 16:11:32 +0200

    riddleofthesphinx ready

--- public/js/riddleofthesphinx_settings.js
+++ public/js/riddleofthesphinx_settings.js
@@ -1,6 +1,6 @@
 var settings = {
-    "symbolSize": 233,
-    "spaceBetweenReels": 35,
+    "symbolSize": 220,
+    "spaceBetweenReels": 30,
 
     "numOfRows": 3,
     "numOfReels": 5,
@@ -16,8 +16,7 @@ var settings = {
     // due to the slow browsers perfomance
     "delayBeforeStartReelsSpin": 200,
     "spinAnimationTimeInMs": 1000,
-    "spinAnimTimingFunc": "ease-in-out",
-    "fallAnimTimingFunc": "cubic-bezier(.79,1.51,.74,.84)",
+
 
     "gamblePreviousCardsAmount": 7,
     "gambleExtended": true,
@@ -29,7 +28,7 @@ var settings = {
         {"image": "1.png"},
         {"image": "2.png"},
         {"image": "3.png"},
-        {"image": "4.png", "animation": "4Anim.png"},
+        {"image": "4.png"},
         {"image": "5.png"},
         {"image": "6.png"},
         {"image": "7.png"},
