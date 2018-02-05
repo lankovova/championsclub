294fb13a46738cc97f1c0cfa4e4da9db9592bd33
lankovova <lanko.vova@gmail.com>
2018-02-05 16:00:24 +0200

    refactoring

--- resources/assets/js/divslot/Translator.js
+++ resources/assets/js/divslot/Translator.js
@@ -9,4 +9,19 @@ function getTranslation() {
     return (settings.gameType === 'old') ? translations['en'] : translations[lang];
 }
 
+// TODO:
+class Translator {
+    constructor() {
+        const langFromCookie = CookieController.get('lang');
+        const lang = langFromCookie ? langFromCookie : 'en';
+
+        // Always return english translation if it is old game
+        this.words = (settings.gameType === 'old') ? translations['en'] : translations[lang];
+    }
+
+    bonusSpinsEnded(spins, win) {
+        // return this.words. ...
+    }
+}
+
 export default getTranslation();
