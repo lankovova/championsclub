fd8b0d362cf2e7312e067f44f4dfbce1eb18ee97
AlexandrKrivosheev <alexandrkrivosheevmail@gmail.com>
2018-02-05 11:51:20 +0200

    nautilus settings ready

--- public/js/nautilus_settings.js
+++ public/js/nautilus_settings.js
@@ -17,8 +17,6 @@ var settings = {
     // due to the slow browsers perfomance
     "delayBeforeStartReelsSpin": 200,
     "spinAnimationTimeInMs": 1000,
-    "spinAnimTimingFunc": "ease-in-out",
-    "fallAnimTimingFunc": "cubic-bezier(.79,1.51,.74,.84)",
 
     "gamblePreviousCardsAmount": 7,
     "gambleExtended": true,
@@ -35,71 +33,71 @@ var settings = {
             //     "image": "1.png",
             //     "frames": 34
             // },
-            "paytable": [0,2,5,25,100],
+            "paytable": [0, 0, 5, 25, 100],
         }, {
             "image": "2.png",
             // "animation": {
             //     "image": "2.png",
             //     "frames": 10
             // },
-            "paytable": [0,0,5,25,100],
+            "paytable": [0, 0, 5, 25, 100],
         }, {
             "image": "3.png",
             // "animation": {
             //     "image": "3.png",
             //     "frames": 22
             // },
-            "paytable": [0,0,10,50,125],
+            "paytable": [0, 0, 10, 50, 125],
         }, {
             "image": "4.png",
             // "animation": {
             //     "image": "4.png",
             //     "frames": 23
             // },
-            "paytable": [0,2,5,20,500],
+            "isScatter": true,
+            "paytable": [0, 2, 5, 20, 500],
         }, {
             "image": "5.png",
             // "animation": {
             //     "image": "5.png",
             //     "frames": 9
             // },
-            "isScatter": true,
-            "paytable": [0,0,20,100,400],
+            "paytable": [0, 0, 20, 100, 400],
         }, {
             "image": "6.png",
             // "animation": {
             //     "image": "6.png",
             //     "frames": 11
             // },
-            "paytable": [0,0,5,25,100],
+            "paytable": [0, 0, 5, 25, 100],
         }, {
             "image": "7.png",
             // "animation": {
             //     "image": "7.png",
             //     "frames": 34
             // },
-            "paytable": [0,0,10,50,125],
+            "paytable": [0, 0, 10, 50, 125],
         }, {
             "image": "8.png",
             // "animation": {
             //     "image": "8.png",
             //     "frames": 11
             // },
-            "paytable": [0,2,25,125,750],
+            "paytable": [0, 2, 25, 125, 750],
         }, {
             "image": "9.png",
             // "animation": {
             //     "image": "9.png",
             //     "frames": 29
             // },
-            "paytable": [0,10,250,2500,9000],
+            "paytable": [0, 10, 250, 2500, 9000],
         }, {
             "image": "10.png",
             // "animation": {
             //     "image": "10.png",
             //     "frames": 36
             // },
-            "paytable": [0,2,25,125,750],
+            "paytable": [0, 2, 25, 125, 750],
         },
         {
             "image": "11.png",
@@ -107,7 +105,7 @@ var settings = {
             //     "image": "11.png",
             //     "frames": 36
             // },
-            "paytable": [0,0,5,25,100],
+            "paytable": [0, 0, 5, 25, 100],
         },
         {
             "image": "12.png",
@@ -115,7 +113,7 @@ var settings = {
             //     "image": "12.png",
             //     "frames": 36
             // },
-            "paytable": [0,0,15,75,250],
+            "paytable": [0, 0, 15, 75, 250],
         },
         {
             "image": "13.png",
@@ -123,7 +121,7 @@ var settings = {
             //     "image": "13.png",
             //     "frames": 36
             // },
-            "paytable": [0,0,15,75,250] ,
+            "paytable": [0, 0, 15, 75, 250],
         },
     ]
 }
