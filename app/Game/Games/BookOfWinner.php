9cd1fbccee2a29d675f2cfb7f6fb994cb9f30079
lankovova <lanko.vova@gmail.com>
2018-02-05 12:48:28 +0200

    fixed book of winner paytables + fixed js float problem

--- app/Game/Games/BookOfWinner.php
+++ app/Game/Games/BookOfWinner.php
@@ -11,14 +11,14 @@ class BookOfWinner extends Game {
     public static $freeSpinsAmount = 10;
     public static $symbolsAmount = 10;
     public static $paytable = [
-        [0, 0, 5, 25, 100],         // Symbol 0
+        [0, 0, 3, 30, 300],         // Symbol 0
         [0, 0, 5, 40, 150],         // Symbol 1
-        [0, 0, 3, 30, 300],         // Symbol 2
+        [0, 0, 5, 25, 100],         // Symbol 2
         [0, 5, 30, 100, 750],       // Symbol 3
         [0, 0, 5, 25, 100],         // Symbol 4
         [0, 0, 5, 40, 150],         // Symbol 5
         [0, 10, 100, 1000, 5000],   // Symbol 6
-        [0, 0, 3, 30, 300],         // Symbol 7
+        [0, 0, 5, 25, 100],         // Symbol 7
         [0, 5, 40, 400, 2000],      // Symbol 8
         [0, 5, 30, 100, 750]        // Symbol 9
     ];
