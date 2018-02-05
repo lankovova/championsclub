96737dcbed0890d2454ca16d1f038a07b5d12bd2
AlexandrKrivosheev <alexandrkrivosheevmail@gmail.com>
2018-02-05 12:08:28 +0200

    luckyladysglamor help added

--- resources/views/games/luckyladysglamor.blade.php
+++ resources/views/games/luckyladysglamor.blade.php
@@ -3,7 +3,29 @@
 @section('css')
     <link href="{{asset('public/css/luckyladysglamor.css')}}" rel=stylesheet type=text/css>
 @endsection
-    
+@section('help')
+
+@component('games.parts.help')
+    @slot('helpValues')
+        <div data-symbols="4 3" class="help__prize_container"></div>
+        <div data-symbols="4 3" data-joker class="help__prize_container"></div>
+        <div data-symbols="9 2" class="help__prize_container"></div>
+        <div data-symbols="9 2" data-joker class="help__prize_container"></div>
+        <div data-symbols="1 8 10" class="help__prize_container"></div>
+        <div data-symbols="1 8 10" data-joker class="help__prize_container"></div>
+        <div data-symbols="5" class="help__prize_container"></div>
+        <div data-symbols="11" class="help__prize_container"></div>
+        <div data-symbols="6 12" class="help__prize_container"></div>
+        <div data-symbols="6 12" data-joker class="help__prize_container"></div>
+        <div data-symbols="7" class="help__prize_container"></div>
+        <div data-symbols="7" data-joker class="help__prize_container"></div>
+        <div data-symbols="0" class="help__prize_container"></div>
+        <div data-symbols="0" data-joker class="help__prize_container"></div>
+
+    @endslot
+@endcomponent
+
+@endsection      
 @section('js')
     @parent
     <script src="{{asset('public/js/luckyladysglamor_settings.js')}}"></script>
