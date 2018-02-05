e0f7dc558f6fd8e2b7cc1a7771617e5e578eefc1
AlexandrKrivosheev <alexandrkrivosheevmail@gmail.com>
2018-02-05 14:01:01 +0200

    beautydolphins paytable checked

--- app/Game/Games/BeautyDolphins.php
+++ app/Game/Games/BeautyDolphins.php
@@ -11,19 +11,19 @@ class BeautyDolphins extends Game {
     public static $freeSpinsAmount = 10;
     public static $symbolsAmount = 13;
     public static $paytable = [
-        [0,0,20,100,400], //PAYTABLE FOR SYMBOL 1
-        [0,0,5,25,100],   //PAYTABLE FOR SYMBOL 2
-        [0,0,10,50,125],    //PAYTABLE FOR SYMBOL 3
-        [0,10,250,2500,9000],    //PAYTABLE FOR SYMBOL 4
-        [0,0,10,75,250],   //PAYTABLE FOR SYMBOL 5
-        [0,0,5,25,100],   //PAYTABLE FOR SYMBOL 6
-        [0,0,5,25,100],   //PAYTABLE FOR SYMBOL 7
-        [0,2,5,25,100],  //PAYTABLE FOR SYMBOL 8
-        [0,0,10,75,250], //PAYTABLE FOR SYMBOL 9
-        [0,2,5,20,500], //PAYTABLE FOR SYMBOL 10(Scatter)
-        [0,2,25,125,750], //PAYTABLE FOR SYMBOL 11
-        [0,2,25,125,750],    //PAYTABLE FOR SYMBOL 12
-        [0,0,10,50,125]    //PAYTABLE FOR SYMBOL 13
+        [0, 0, 20, 100, 400], //PAYTABLE FOR SYMBOL 0
+        [0, 0, 5, 25, 100],   //PAYTABLE FOR SYMBOL 1
+        [0, 0, 10, 50, 125],    //PAYTABLE FOR SYMBOL 2
+        [0, 10, 250, 2500, 9000],    //PAYTABLE FOR SYMBOL 3
+        [0, 0, 15, 75, 250],   //PAYTABLE FOR SYMBOL 4
+        [0, 0, 5, 25, 100],   //PAYTABLE FOR SYMBOL 5
+        [0, 0, 5, 25, 100],    //PAYTABLE FOR SYMBOL 6
+        [0, 2, 5, 25, 100],  //PAYTABLE FOR SYMBOL 7
+        [0, 0, 15, 75, 250], //PAYTABLE FOR SYMBOL 8
+        [0, 2, 5, 20, 500], //PAYTABLE FOR SYMBOL 9
+        [0, 2, 25, 125, 750], //PAYTABLE FOR SYMBOL 10
+        [0, 2, 25, 125, 750],    //PAYTABLE FOR SYMBOL 11
+        [0, 0, 10, 50, 125]    //PAYTABLE FOR SYMBOL 12
     ];
 
     function __construct($game) {
