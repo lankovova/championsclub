15fafd0c3e56a747b5b704cd982219015f36bccc
lankovova <lanko.vova@gmail.com>
2018-02-05 14:21:18 +0200

    computer world paytables done

--- public/js/computerworldold_settings.js
+++ public/js/computerworldold_settings.js
@@ -28,80 +28,38 @@ var settings = {
     "symbolsImagesPath": "public/img/games/computerworldold/symbols/",
     "symbolsAnimationsPath": "public/img/games/computerworldold/symbols/animations/",
 
-    "lines": [1, 2, 3, 4, 5, 6, 7, 8, 9],
-
     "symbols": [
         {
             "image": "1.png",
-            // "animation": {
-            //     "image": "1.png",
-            //     "frames": 34
-            // },
             "paytable": [0,0,5,25,100]
         }, {
             "image": "2.png",
-            // "animation": {
-            //     "image": "2.png",
-            //     "frames": 10
-            // },
             "paytable": [0,0,5,40,150]
         }, {
             "image": "3.png",
-            // "animation": {
-            //     "image": "3.png",
-            //     "frames": 22
-            // },
             "paytable": [0,5,40,400,2000]
         }, {
             "image": "4.png",
-            // "animation": {
-            //     "image": "4.png",
-            //     "frames": 23
-            // },
             "paytable": [0,10,100,1000,5000]
         }, {
             "image": "5.png",
-            // "animation": {
-            //     "image": "5.png",
-            //     "frames": 9
-            // },
-            "isScatter": true,
             "paytable": [0,0,5,25,100]
         }, {
             "image": "6.png",
-            // "animation": {
-            //     "image": "6.png",
-            //     "frames": 11
-            // },
             "paytable": [0,0,5,40,150]
         }, {
             "image": "7.png",
-            // "animation": {
-            //     "image": "7.png",
-            //     "frames": 34
-            // },
+            "isScatter": true,
             "paytable": [0,0,3,30,300]
         }, {
             "image": "8.png",
-            // "animation": {
-            //     "image": "8.png",
-            //     "frames": 11
-            // },
             "paytable": [00,5,30,100,750]
         }, {
             "image": "9.png",
-            // "animation": {
-            //     "image": "9.png",
-            //     "frames": 29
-            // },
             "paytable": [0,5,30,100,750]
         }, {
             "image": "10.png",
-            // "animation": {
-            //     "image": "10.png",
-            //     "frames": 36
-            // },
-            "paytable": [0,0,5,40,150]
+            "paytable": [0,0,5,25,100]
         },
     ],
     "linePresenter": [
